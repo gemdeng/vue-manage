18a3d4028cf053a6b5cd2619a5355e7298df6805
tancheng <781977852@qq.com>
2022-10-07 10:58:23 +0800

    master

---
+++ src/views/dashboard/Observe/Search/lineCharts/index.vue
@@ -0,0 +1,123 @@
+<template>
+  <div>
+    <div class="header">
+      <span class="search-header">搜索用户数</span>
+      <svg
+        t="1637478175000"
+        class="icon"
+        viewBox="0 0 1024 1024"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        p-id="2179"
+        width="26"
+        height="26"
+      >
+        <path
+          d="M536 480v192a16 16 0 0 1-16 16h-16a16 16 0 0 1-16-16V480a16 16 0 0 1 16-16h16a16 16 0 0 1 16 16z m-32-128h16a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16h-16a16 16 0 0 1-16-16v-32a16 16 0 0 1 16-16z m8 448c159.056 0 288-128.944 288-288s-128.944-288-288-288-288 128.944-288 288 128.944 288 288 288z m0 48c-185.568 0-336-150.432-336-336s150.432-336 336-336 336 150.432 336 336-150.432 336-336 336z"
+          p-id="2180"
+          fill="#8a8a8a"
+        ></path>
+      </svg>
+    </div>
+    <div class="main">
+      <span class="main-title">12321</span>
+      <span class="main-content">17.1</span>
+      <i class="el-icon-caret-top" style="color: greenyellow"></i>
+      <i class="el-icon-caret-bottom" style="color: red"></i>
+    </div>
+    <div class="footer">
+      <div class="charts" ref="charts"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import echarts from "echarts";
+
+export default {
+  mounted() {
+    //初始化echarts实例
+    let lineCharts = echarts.init(this.$refs.charts);
+    //配置数据
+    lineCharts.setOption({
+      xAxis: {
+        //隐藏x轴
+        show: false,
+        //均分
+        type: "category",
+      },
+      yAxis: {
+        //隐藏y轴
+        show: false,
+      },
+      //系列
+      series: [
+        {
+          type: "line",
+          data: [10, 7, 25, 12, 38, 9, 29, 70, 44, 64, 22],
+          //拐点的样式的设置
+          itemStyle: {
+            opacity: 0,
+          },
+          //线条的样式
+          lineStyle: {
+            color: "bule",
+          },
+          //填充颜色的设置
+          areaStyle: {
+            color: {
+              type: "linear",
+              x: 0,
+              y: 0,
+              x2: 0,
+              y2: 1,
+              colorStops: [
+                {
+                  offset: 0,
+                  color: "blue", // 0% 处的颜色
+                },
+                {
+                  offset: 1,
+                  color: "#fff", // 100% 处的颜色
+                },
+              ],
+              global: false, // 缺省为 false
+            },
+          },
+          smooth: true,
+        },
+      ],
+      //布局调试
+      grid: {
+        top: "0px",
+        left: "0px",
+        right: "0px",
+        bottom: "0px",
+      },
+    });
+  },
+};
+</script>
+
+<style>
+.header {
+  display: flex;
+  align-items: center;
+}
+.search-header {
+  margin-right: 20px;
+}
+.main {
+  margin: 10px 0px;
+}
+.main-title {
+  margin-right: 30px;
+}
+.main-content {
+  margin-right: 10px;
+}
+.charts {
+  width: 100%;
+  height: 50px;
+}
+</style>
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -2,18 +2,21 @@
   <div>
     <Card></Card>
     <Sale></Sale>
+    <Observe></Observe>
   </div>
 </template>
 
 <script>
 import Card from "./Card";
 import Sale from "./Sale";
+import Observe from "./Observe";
 
 export default {
   name: "",
   components: {
     Card,
     Sale,
+    Observe,
   },
 };
 </script>
